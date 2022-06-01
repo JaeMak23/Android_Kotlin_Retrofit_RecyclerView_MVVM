b6bb1f00cd2e561961a9b2289699a9bf76f725e9
jaemak23 <macklin.jaison@gmail.com>
2022-06-01 13:16:38 +0530

    Created 3 data classes[User, UserList, UserResponse]

---
+++ app/src/main/java/com/example/androidkotlinretrofitrecyclerviewmvvm/UserList.kt
@@ -0,0 +1,3 @@
+package com.example.androidkotlinretrofitrecyclerviewmvvm
+
+data class UserList(val data:List<User>)
---
+++ app/src/main/java/com/example/androidkotlinretrofitrecyclerviewmvvm/UserResponse.kt
@@ -0,0 +1,6 @@
+package com.example.androidkotlinretrofitrecyclerviewmvvm
+
+data class UserResponse(
+    val code:Int?,
+    val meta:String,
+    val data: User)
